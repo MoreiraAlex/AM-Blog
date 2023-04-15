45388b4c27646ba54157117d6c1b4b370374450f
MooreiraAlex <amoreira@allianceconsultoria.com.br>
2023-04-14 23:50:29 -0300

    Removendo projeto antigo

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import HeaderHome from './layout/headerHome';
-import Flex from './flex';
-import Container from './container';
-import CategorySlide from './layout/Category';
-
-export { HeaderHome, Flex, Container, CategorySlide };
